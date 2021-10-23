1c5eb23b0814e9fc368ba506446962c4d9bbb2e7
sheik <sheikalaudeen199@gmail.com>
2021-10-23 11:13:37 +0530

    First commit

---
+++ details.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class student
+{
+public static void main(String args[])
+{
+    Scanner s=new Scanner(System.in());
+    int age;
+    long adhar;
+    String name,roll,f,m,email,blood,add;
+     System.out.println("enter the name");
+    name=s.next();
+    System.out.println("enter the roll number");
+    roll=s.nextInt();
+    System.out.println("enter father's name:);
+    f=s.next();
+   System.out.println("enter the mothers name");
+   m=s.next();
+    System.out.println ("enter the age:");
+    
+    age = s.nextInt ();
+     System.out.println ("enter the phn number:");
+     
+    phn = s.nextLong ();
+      
+   System.out.println ("enter the email");
+      
+   email = s.next ();
+      
+   System.out.println ("enter the blood group:");
+     
+   blood = s.next ();
+     
+   System.out.println ("enter the address:");
+     
+    add = s.next ();
+
