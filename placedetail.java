4438d76e66fe16829be9b9a4fdb7cad28d0083e9
sheik <sheikalaudeen199@gmail.com>
2021-10-23 11:43:16 +0530

    placement commit

---
+++ placedetail.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class placement 
+{
+public static void main(String args[])
+{
+    Scanner s=new Scanner(System.in());
+    System.out.println("Student name:");
+    String n=s.next();
+    System.out.println("Roll number :");
+    String n1=s.next();
+    System.out.println("mobile number:");
+    long n2=s.nextLong();
+    System.out.println("Personal mail:");
+    String n3=s.next();
+    System.out.println("Edu mail:");
+    String n4=s.next();
+     System.out.println("number of arrear :");
+    int n5=s.nextInt();
+    System.out.println("gpa 1St sem:");
+    int n5=s.nextInt();
+     System.out.println("gpa 2St sem:");
+    int n5=s.nextInt();
+    System.out.println("gpa 3St sem:");
+    int n5=s.nextInt();
+   S
+}
+}
+   
