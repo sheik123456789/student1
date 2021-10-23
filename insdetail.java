a78e9677bc2ac059707cabac034612261417f557
sheik <sheikalaudeen199@gmail.com>
2021-10-23 11:33:50 +0530

    insurance commit

---
+++ insdetail.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class insurance
+{
+public static void main(String args[])
+{
+    Scanner s=new Scanner(System.in());
+    System.out.println("Student name:");
+    String n=s.next();
+    System.out.println("Nominee name:");
+    String n1=s.next();
+    System.out.println("relationship of Nominee:");
+    String n2=s.next();
+    System.out.println("Nominee age:");
+    int n3=s.nextInt();
+    System.out.println("Nominee mobile number:");
+    long n4=s.nextLong();
+    System.out.println("nominee Occupation:");
+    String n5=s.next();
+    System.out.println("Nominee email:");
+    String n6=s.next();
+    System.out.println("Nominee aadhar name :");
+    long n=s.nextLong();
+}
+}
+   
