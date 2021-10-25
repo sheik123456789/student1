bd5542e746ef2453be6abc50896b6a7fda3810aa
sheik <sheikalaudeen199@gmail.com>
2021-10-25 19:28:53 +0530

    course completed

---
+++ coursedetail.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class student
+{
+public static void main(String args[])
+{
+    Scanner s=new Scanner(System.in());
+     System.out.println("Roll number:");
+    int  a1=s.next();
+    System.out.println("Semester:");
+     int r2=s.nextInt();
+    System.out.println("No of one credit course completed:");
+    int  r3=s.nextInt();
+   System.out.println("Name of one credit course completed:");
+    String r4=s.next();
+    System.out.println ("No of value added course completed:");
+    
+    int r5 = s.nextInt ();
+    System.out.println ("Name of value added course: ");
+     
+    String r6= s.next();
+      
+}
+}
+   
