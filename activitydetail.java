fd8436dfcd1c1c0fc99b3cf09eb233ca853c9324
sheik <sheikalaudeen199@gmail.com>
2021-10-25 19:23:46 +0530

    activiy commit

---
+++ activitydetail.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+class student
+{
+public static void main(String args[])
+{
+    Scanner s=new Scanner(System.in());
+     System.out.println("Roll number:");
+    int  a1=s.next();
+    System.out.println("Semester:");
+     int r2=s.nextInt();
+    System.out.println("No of paper presented inside KEC:");
+    int  r3=s.nextInt();
+    System.out.println("No of project presented inside KEC:");
+    int  r4=s.nextInt();
+   System.out.println("No of paper presented outside KEC:");
+    int  r5=s.nextInt();
+   System.out.println("No of project presented outside KEC:");
+    int  r6=s.nextInt();
+    System.out.println ("No of prize won in coding carnival:");
+    
+    int r7= s.nextInt ();
+    System.out.println ("No of prize won in hackathon :");
+     
+    String r8= s.next();
+      
+}
+}
+   
