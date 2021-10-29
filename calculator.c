aedc8e02e5ee3b71a22171ed8fe90d7583ef7823
sheik <sheikalaudeen199@gmail.com>
2021-10-29 19:41:36 +0530

    calculator

---
+++ calculator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f,g,h;
+   scanf("%d",&SSSa);
+   scanf("%d",&b);
+   c=a+b;
+    printf("sum of two number is %d",c);
+   d=a-b;
+printf("differance of two number is :%d",d);
+
+
+    e=a*b;
+   printf("mutiplication of two number is :%d",e);
+   if(b==0)
+      printf("zero cannot be divisible);
+  else
+      g=a/b;
+   printf("divisible of two number %d",g);
+}
